f1b264b4cb44ced62ec2fb3d33aaa27273504a29
Pavan Kumar <135621512+Pavan-Kumar-18@users.noreply.github.com>
2023-10-03 23:40:24 +0530

    Update index.js
    intial commit

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const Input = document.querySelector("input")
-const Button = document.querySelector("button")
-const Output = document.querySelector("span")
+const Input = document.querySelector("input");
+const Button = document.querySelector("button");
+const Output = document.querySelector("span");
 
-function randomNumber(){
-    const Random = Math.floor(Math.random()*100)
-    const Value = Input.value
+function randomNumber() {
+  const Random = Math.floor(Math.random() * 100) + 1; // Generate a random number between 1 and 100
+  const Value = parseInt(Input.value); // Parse the input value to an integer
 
-let atempt = 0
-if (isNaN(Value) || Value < 1 || Value > 100) {
+  if (isNaN(Value) || Value < 1 || Value > 100) {
     Output.textContent = "Please enter a valid number between 1 and 100.";
     Input.focus();
     return;
+  }
+
+  if (Value === 18) {
+    Output.textContent = "Congratulations! You guessed the number 🎇🎉🎉";
+  } else {
+    Output.textContent = "Random number was " + Random + " 😥";
+  }
 }
 
+Button.addEventListener("click", () => {
+  randomNumber();
+});
 
-    if(Value ===18){
- Output.textContent  = "Congralation You Guss the number🎇🎉🎉" 
-    }else{
-        Output.textContent = "Random number was  " + Random + "😥"
-    }
-
-
-
-Button.addEventListener("click",()=>{
-    randomNumber()
-})
